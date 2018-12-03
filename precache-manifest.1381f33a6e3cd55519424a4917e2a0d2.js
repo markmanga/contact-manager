6a5d1ca190b36f09e6935d6962f75a651c2e382a
Mark Manga <m.mangawang@protonmail.com>
2018-12-04 03:10:36 +0800

    Updates

---
+++ precache-manifest.1381f33a6e3cd55519424a4917e2a0d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contact-manager/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "5b5c1af730d170226de4",
+    "url": "/contact-manager/static/js/main.5b5c1af7.chunk.js"
+  },
+  {
+    "revision": "d6aa3ea4654a9cee9880",
+    "url": "/contact-manager/static/js/1.d6aa3ea4.chunk.js"
+  },
+  {
+    "revision": "5b5c1af730d170226de4",
+    "url": "/contact-manager/static/css/main.a616d996.chunk.css"
+  },
+  {
+    "revision": "da37c34f9fab18471da2dd08dc7c936d",
+    "url": "/contact-manager/index.html"
+  }
+];
